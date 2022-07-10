6cf684d8cdf761cac64b0f1a663de73d6223b3bf
SamuelPrigent <80969347+SamuelPrigent@users.noreply.github.com>
2022-07-10 14:56:35 +0200

    le modal peut désormais être fermé via la croix

--- modal.js
+++ modal.js
@@ -1,3 +1,4 @@
+//
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -8,16 +9,23 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
+const modalbg = document.querySelector(".bground"); // Modal div
+const modalBtn = document.querySelectorAll(".modal-btn"); // Ecoute Clic => "Je m'inscris"
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal)); // Appel la fonction ouvrir
 
-// launch modal form
+// launch modal form // Display "block" remplace le => none et affiche le modal
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close Modal
+const modalClose = document.querySelectorAll(".close");
+modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
+// FormData - Stock des données du formulaire
